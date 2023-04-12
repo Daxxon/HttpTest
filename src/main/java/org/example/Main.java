e5de7f0be6ad0ca280f655402e38288d917e9077
Daniel Waggoner <daxxon37@gmail.com>
2023-04-12 15:49:06 -0400

    updated to return list of vins

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,56 +1,38 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+
+import java.io.*;
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-//        Map<String, String> formData = new HashMap<>();
-//        formData.put("name", "dwaggoner@blackwidowimaging.com");
-//        formData.put("pass", "3k=4;sP4BMhp");
-//        formData.put("form_build_id", "form-a0vRrFU65mSjE_ny8w_rvsv5aveRFa8Z99m3bLeB33g");
-//        formData.put("form_id", "user_login");
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        String requestBody = objectMapper
-//                .writeValueAsString(formData);
-//
-//        System.out.println(objectMapper.writeValueAsString(formData));
-//
-//        HttpRequest httpRequest = HttpRequest.newBuilder()
-//                .uri(URI.create("https://dealers.3iblackwidow.com/poage-chevrolet-buick.csv"))
-//                .header("Accept-Encoding", "gzip,deflate,br")
-//                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-//                .build();
-//
-//        HttpResponse<String> response = HttpClient.newBuilder()
-//                .build()
-//                .send(httpRequest, HttpResponse.BodyHandlers.ofString());
-//
-//        System.out.println("Response: " + response.toString());
-        char[] array = new char[1000];
         InputStream input = new URL("https://dealers.3iblackwidow.com/Octane-Buick-GMC.csv").openStream();
-        Reader reader = new InputStreamReader(input, "UTF-8");
-        reader.read(array);
-
-        StringBuilder myString = new StringBuilder();
-        for (char c : array) {
-            myString.append(c);
-        }
-
-        System.out.println("Data in the stream:");
-        System.out.println(myString);
-        // Closes the reader
+        Scanner scanner = new Scanner(input).useDelimiter("/n");
+        String result = scanner.hasNext() ? scanner.next() : "";
         input.close();
+
+        BufferedReader bufferedReader = new BufferedReader(new StringReader(result));
+        String line = null;
+        List<String> vins = new ArrayList<>();
+        Map<String,String> individualVehicle = new HashMap<>();
+        String[] keys = bufferedReader.readLine().split("\",\"");
+        while ((line = bufferedReader.readLine()) != null){
+            individualVehicle = new HashMap<>();
+            String[] lineArr = line.split("\",\"");
+            for (int i=0; i<(lineArr.length-1); i++) {
+                individualVehicle.put(keys[i].toString().replace("\"", ""), lineArr[i].toString().replace("\"",""));
+            }
+            vins.add(individualVehicle.get("VIN"));
+        }
+        bufferedReader.close();
+        for (String vin : vins) {
+            System.out.println(vin);
+        }
     }
 }
